ad4053a408bbde3ef08b70213b3b00057a2e4fd5
Sam Kim <tskim218@hotmail.co>
2020-05-27 23:23:41 -0600

    add shutdown

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/tskim218/HashEncodePassword/datasource"
+	"github.com/tskim218/HashEncodePassword/handlers"
 	"log"
 	"net/http"
-
-	"github.com/tskim218/HashEncodePassword/handlers"
-	"github.com/tskim218/HashEncodePassword/datasource"
+	"time"
 )
 
 func main() {
+
+	messages := make(chan string)
+
 	// this creates the backend storage system
 	db := datasource.NewInMemoryDB()
 	// this creates a new http.ServeMux, which is used to register handlers to execute in response to routes
@@ -20,9 +25,31 @@ func main() {
 
 	log.Printf("serving on port 8080")
 
+	srv := &http.Server{
+		Handler: mux,
+		Addr:    fmt.Sprintf(":%s", "8080"),
+	}
+
 	// http.ListenAndServe takes in an http.Handler as its second parameter.
 	// since ServeMux implements a ServeHTTP function, it is also an http.Handler,
 	// so we can pass it here.
-	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err)
+	//err := http.ListenAndServe(":8080", mux)
+	go func() {
+		err := srv.ListenAndServe()
+		log.Fatal(err)
+	}()
+
+	go func() { messages <- "ping" }()
+
+	time.Sleep(10*time.Second)
+
+	<-messages
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("error shutting down server %s", err)
+	} else {
+		log.Println("Server gracefully stopped")
+	}
 }
